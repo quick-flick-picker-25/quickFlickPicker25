a0c1365ab094f6ea20bd85e6c4ac35a6d10cf514
Kay Evans-Stocks <kay.stocks@rogers.com>
2020-03-05 18:54:19 -0500

    added ability to push names to firebase

--- src/Lists.js
+++ src/Lists.js
@@ -7,6 +7,7 @@ class Lists extends Component {
         this.state = {
             dbRef: firebase.database().ref(),
             usersList: [],
+            userListName: '',
         }
     }
 
@@ -16,7 +17,7 @@ class Lists extends Component {
         this.state.dbRef.on('value', (response) => {
             const data = response.val();
 
-            console.log(response);
+            console.log(data);
             //make empty array
             const stateToSet = []
             //loop through our data
@@ -35,12 +36,36 @@ class Lists extends Component {
         })
     }
 
+    // logs the changes when user types into input
+    handleUserInput = (e)=>{
+        this.setState({
+            userListName: e.target.value,
+        })
+    }
+
+    // make a function that records the value of the user input
+    handleUserListName = (e) => {
+        // prevent default action
+        e.preventDefault();
+
+        // create new reference point in database
+        const newList = firebase.database().ref(this.state.userListName);
+
+        // push the name on submit to create node in firebase
+        newList.push(this.state.userListName);
+
+        // set to empty string 
+        this.setState({
+            userListName: '',
+        })
+    }
+
     render() {
         return (
             <div>
                 <h2>Your Lists</h2>
-                <form action="">
-                    <input type="text" placeholder="New list name" />
+                <form action="" onSubmit={this.handleUserListName}>
+                    <input onChange={this.handleUserInput} type="text" placeholder="New list name" value={this.state.userListName}/>
                     <button type="submit">Submit</button>
                 </form>
             </div>
