9d30fa90525edfaf55b55866596357683cb69846
Opjot Singh Nijjar <opjot-singh-nijjar@Opjots-MacBook-Pro.local>
2020-03-05 16:35:57 -0500

    added firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import firebase from './firebase'
 
 class App extends Component {
   render() {
