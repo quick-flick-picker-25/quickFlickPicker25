510dcf7dcf6279c983b48d3822dbbfd77bfba4ff
Ariel Rivera <rivera182@gmail.com>
2020-03-06 15:40:30 -0500

    displayed movie details on to poster

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -13,6 +13,8 @@ class MovieDetails extends Component {
 
         this.state = {
             movie: {},
+            movieGenre: [],
+            credits: {}
         }
     }
 
@@ -27,17 +29,65 @@ class MovieDetails extends Component {
 
             this.setState({
                 movie: movie,
+                movieGenre: movie.genres,
             })
 
-            console.log(movie)
+            console.log(this.state.movieGenre)
         })
+
+        axios({
+            url: `https://api.themoviedb.org/3/movie/${this.props.match.params.movieID}/credits`,
+            params: {
+                api_key: '8341ba99fae06408554c7e8411e4a4f9',
+            }
+        }).then(response => {
+            const  credits = response.data;
+
+            const director = credits.filter((job) => {
+                
+            })
+            
+
+            // this.setState({
+            //     movie: movie,
+            //     movieGenre: movie.genres,
+            // })
+
+            // console.log(this.state.movie)
+        })
+
     }
 
     render(){
+    
+        // console.log(this.state.movie.genres);
+        // const genres = this.state.movie.genres;
+
         return (
             <section className="movieDetails">
                 <Link to="/">Back to results</Link>
-                <h1>{this.state.movie.title}</h1>
+                <div>
+                    <img src={`http://image.tmdb.org/t/p/w500/${this.state.movie.poster_path}`} alt=""/>
+                </div>
+
+                <div>
+                    <h1>{this.state.movie.title}</h1>
+                    <div className="genres">
+                        <h2>Genres</h2>
+                        {/* {
+                            this.state.movie.genres.map((genre, index) => {
+                                return (
+                                    <p key={index}>{genre.name}</p>
+                                )
+                            })
+                        } */}
+                    </div>
+                    <div className="director">
+                        <h2>Director</h2>
+                     <p>{this.state.movie.director}</p>
+                    </div>
+                </div>
+
             </section>
         )
     }
