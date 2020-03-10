9be8c95303095e09a6afa823e4aafb4f956d4549
Ariel Rivera <rivera182@gmail.com>
2020-03-10 10:19:15 -0400

    list styleing

--- src/Lists.js
+++ src/Lists.js
@@ -90,40 +90,48 @@ class Lists extends Component {
 
     render() {
         return (
-            <div>
-                <h2>Your Lists</h2>
-                <form action="" onSubmit={this.handleUserListName}>
-                    <label htmlFor="listName">Please enter a list name</label>
-                    <input onChange={this.handleUserInput} type="text" id="listName" placeholder="New list name" value={this.state.userListName}/>
-                    <button type="submit">Submit</button>
-                </form>
-                <ul>
-                    {
-                        this.state.usersList.map((list)=>{
-                            return(
-                                <li key={list.key}>
-                                    <h3>{list.key}</h3>
-                                    <div className="movies">
-                                        <a className="showMovies" href="" onClick={this.handleReload}>see list.</a>
-                                        <ul className="moviesDisplayed">
-                                            {this.handleMovieName(list).map((movie, index) => {
-                                                return(
-                                                    <li key={index}>
-                                                        <p>{movie}</p>
-                                                        <button>Delete</button>
-                                                    </li>
-                                                )
-                                            })}
-                                        </ul>
-                                    </div>
-                                    {/* <Link to={`/watch-movie/${list.key}`}>Watch Movie</Link> */}
-                                    <Link to={{ pathname: `/watch-movie/`, state: {specificList: list.key}}}>Watch Movie</Link>
-                                    <button onClick={()=>{this.handleDeleteList(list.key)}}>delete list.</button>
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
+            <div className="yourLists">
+                <div className="wrapper">
+                    <h2>Your Lists:</h2>
+                    <form className="listInput" action="" onSubmit={this.handleUserListName}>
+                        <label className="labelHidden" htmlFor="listName">Please enter a list name</label>
+                        <input onChange={this.handleUserInput} type="text" id="listName" placeholder="New list name" value={this.state.userListName}/>
+                        <button className="roundButton" type="submit">
+                            <i class="fas fa-plus"></i>
+                        </button>
+                    </form>
+                    <ul>
+                        {
+                            this.state.usersList.map((list)=>{
+                                return(
+                                    <li className="dropDownList" key={list.key}>
+                                        <h3>{list.key}</h3>
+                                        <div className="movies">
+                                            <a className="showMovies" href="" onClick={this.handleReload}>
+                                                <i class="fas fa-chevron-down"></i>
+                                            </a>
+                                            <ul className="moviesDisplayed">
+                                                {this.handleMovieName(list).map((movie, index) => {
+                                                    return(
+                                                        <li key={index}>
+                                                            <p>{movie}</p>
+                                                            <button>
+                                                                <i class="fas fa-trash-alt"></i>
+                                                            </button>
+                                                        </li>
+                                                    )
+                                                })}
+                                            </ul>
+                                        </div>
+                                        {/* <Link to={`/watch-movie/${list.key}`}>Watch Movie</Link> */}
+                                        <Link to={{ pathname: `/watch-movie/`, state: {specificList: list.key}}}>Watch Movie</Link>
+                                        <button onClick={() => { this.handleDeleteList(list.key) }}><i class="fas fa-trash-alt"></i></button>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
             </div>
         )
     }
