847df74590fa1250e56b4ca29cd647e838ab4646
abir-halwa <abeer_h83@hotmail.com>
2020-03-10 14:59:42 -0400

    Updates

---
+++ precache-manifest.00b7cb778d5a6327d2d24753e7b6e23b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38396118d5f18c4485d456686dfdd5b4",
+    "url": "/quick-flick-picker-25/quickFlickPicker25/index.html"
+  },
+  {
+    "revision": "ea72576692b941e1b245",
+    "url": "/quick-flick-picker-25/quickFlickPicker25/static/css/main.a6bd8612.chunk.css"
+  },
+  {
+    "revision": "20ce7bd453aa8dcb0f22",
+    "url": "/quick-flick-picker-25/quickFlickPicker25/static/js/2.8dee2a1d.chunk.js"
+  },
+  {
+    "revision": "7ba5a91c6aa4ae9cc068a25fb13d169c",
+    "url": "/quick-flick-picker-25/quickFlickPicker25/static/js/2.8dee2a1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea72576692b941e1b245",
+    "url": "/quick-flick-picker-25/quickFlickPicker25/static/js/main.17cbb602.chunk.js"
+  },
+  {
+    "revision": "c0cd4b190287e1718066",
+    "url": "/quick-flick-picker-25/quickFlickPicker25/static/js/runtime-main.611c1adf.js"
+  }
+]);
