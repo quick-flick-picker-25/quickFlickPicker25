ac922a998651d301768d5db2c5edbce778da0557
abir-halwa <abeer_h83@hotmail.com>
2020-03-10 20:49:56 -0400

    Updates

---
+++ precache-manifest.9fa66569e316289db1aa4e4c95688d76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "420ba59a9bf5e018d02a4d867ebbac21",
+    "url": "/quickFlickPicker25/index.html"
+  },
+  {
+    "revision": "68f00b9a6d7a78b506e4",
+    "url": "/quickFlickPicker25/static/css/main.a6bd8612.chunk.css"
+  },
+  {
+    "revision": "dd88de6af2113ddd71ab",
+    "url": "/quickFlickPicker25/static/js/2.22868b86.chunk.js"
+  },
+  {
+    "revision": "7ba5a91c6aa4ae9cc068a25fb13d169c",
+    "url": "/quickFlickPicker25/static/js/2.22868b86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68f00b9a6d7a78b506e4",
+    "url": "/quickFlickPicker25/static/js/main.a63b6fa7.chunk.js"
+  },
+  {
+    "revision": "2db411ed65f03820ceec",
+    "url": "/quickFlickPicker25/static/js/runtime-main.8cbaaf37.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bb85f44239f00ac4ccc51e82a8a5f6c0.js"
+  "/quickFlickPicker25/precache-manifest.9fa66569e316289db1aa4e4c95688d76.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quickFlickPicker25/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
